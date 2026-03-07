e8c150453bdd48a987261e37ac1e1aa2c40cdd90
PoshX1z <xdante1357@gmail.com>
2026-03-07 15:52:31 +0700

    fix: favorite not display

--- components/PostList.jsx
+++ components/PostList.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import PostCard from "./PostCard";
 import PostCount from "./PostCount";
 
-function PostList({ posts }) {
+function PostList({ posts, favorites, onToggleFavorite }) {
   const [search, setSearch] = useState("");
 
-  const filtered = posts.filter((post) => post.title.toLowerCase().includes(search.toLocaleLowerCase()))
+  const filtered = posts.filter((post) =>
+    post.title.toLowerCase().includes(search.toLocaleLowerCase()),
+  );
   return (
     <div>
       <h2
@@ -16,26 +18,38 @@ function PostList({ posts }) {
         }}
       >
         โพสต์ล่าสุด
-        </h2>
+      </h2>
 
-        <input type="text" placeholder="ค้นหาโพสต์ที่ต้องการ..." value={search} onChange={(e) => setSearch(e.target.value)} style={{
+      <input
+        type="text"
+        placeholder="ค้นหาโพสต์ที่ต้องการ..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        style={{
           width: "100%",
           padding: "0.5rem 0.75rem",
           border: "1px solid #cbd5e0",
           borderRadius: "6px",
           fontSize: "1rem",
           marginBottom: "1rem",
-          boxSizing: "border-box"
-        }}/>
+          boxSizing: "border-box",
+        }}
+      />
 
-        {filtered.length == 0 && (
-          <p style={{color:"#718096", textAlign:"center", padding: "2rem"}}>
-            ไม่พบโพสต์ที่ต้องการ
-          </p>
-        )}
-    {filtered.map((post) => (
-      <PostCard key={post.id} title={post.title} body={post.body}/>
-    ))}
+      {filtered.length == 0 && (
+        <p style={{ color: "#718096", textAlign: "center", padding: "2rem" }}>
+          ไม่พบโพสต์ที่ต้องการ
+        </p>
+      )}
+      {filtered.map((post) => (
+        <PostCard
+          key={post.id}
+          title={post.title}
+          body={post.body}
+          isFavorite={favorites.includes(post.id)}
+          onToggleFavorite={() => onToggleFavorite(post.id)}
+        />
+      ))}
     </div>
   );
 }
