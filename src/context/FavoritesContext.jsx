5e4f92c7a4dcf3997a5f657027c0154f84f80c56
Pratchaya Leelanuwat <67160017@go.buu.ac.th>
2026-03-18 10:52:34 +0700

    chore: create favorite context

---
+++ src/context/FavoritesContext.jsx
@@ -0,0 +1,23 @@
+const favoritesContext = createContext();
+
+export function FavoritesProvider({ children }) {
+  const [favorites, setFavorites] = useState([]);
+
+  const toggleFavorite = (postId) => {
+    setFavorites((prev) =>
+      prev.includes(postId)
+        ? prev.filter((id) => id !== postId)
+        : [...prev, postId],
+    );
+  };
+
+  return (
+    <FavoritesContext.Provider value={{ favorites, toggleFavorite }}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+}
+
+export function useFavorites() {
+  return useContext(FavoritesContext);
+}
