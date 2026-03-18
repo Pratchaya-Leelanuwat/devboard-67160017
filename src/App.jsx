f9a16bd530895203c74664178eb3ca990186806a
Pratchaya Leelanuwat <67160017@go.buu.ac.th>
2026-03-18 17:57:36 +0700

    feat: search page when type in url

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import ProfilePage from "./pages/ProfilePage";
 import FavoritesPage from "./pages/FavoritesPage";
 import { FavoritesProvider } from "./context/FavoritesContext";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useSearchParams,
+} from "react-router-dom";
 import PostDetailPage from "./pages/PostDetailPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import SearchPage from "./pages/SearchPage";
 
 export default function App() {
   return (
@@ -15,10 +21,11 @@ export default function App() {
         <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route path="/" element={<HomePage />} />x
+            <Route path="/" element={<HomePage />} />
             <Route path="/posts/:id" element={<PostDetailPage />} />
             <Route path="/profile" element={<ProfilePage />} />
             <Route path="/favorites" element={<FavoritesPage />} />
+            <Route path="/search" element={<SearchPage />} />
 
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,29 @@
+import { useSearchParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import PostCard from "../../components/PostCard";
+
+function SearchPage() {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("q") || "";
+
+  const { data } = useFetch("https://jsonplaceholder.typicode.com/posts");
+  const filteredPosts = data.filter((post) =>
+    post.title.toLowerCase().includes(query.toLocaleLowerCase()),
+  );
+
+  return (
+    <div className="max-w-3xl mx-auto my-5">
+      <h1 className="text-center text-2xl my-5">ผลการค้นหาสำหรับ "{query}"</h1>
+      {filteredPosts.length > 0 ? (
+        <div>
+          {filteredPosts.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
+      ) : (
+        <p>ไม่พบโพสต์ที่ตรงกับคำค้นหา</p>
+      )}
+    </div>
+  );
+}
+export default SearchPage;
